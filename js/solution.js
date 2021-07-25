946da8aa31c85fc28f6c5bce5734941231d450ba
CatsPsychologist <mainofficee@gmail.com>
2021-07-25 19:52:14 +0300

    upd

--- js/solution.js
+++ js/solution.js
@@ -2,12 +2,32 @@ let bodyEl = document.body
 
 let bodyChilds = bodyEl.childNodes
 
-console.log(bodyChilds)
+console.log(bodyChilds,)
 for(let node of bodyChilds){
     console.log(node)
+    console.log(node.childNodes)
+
 
 }
+
+
 
 const ul = bodyChilds[1]
 
 console.log(ul.childNodes)
+
+let newArray = Array.from(ul.childNodes)
+
+console.log(newArray)
+
+let filterArray = newArray.filter(
+    function (item){
+        if(item.nodeName === 'LI'){
+            return item;
+        }
+        return null
+    }
+)
+
+console.log(filterArray)
+console.log(filterArray.length)
